68c8190222c0931b6dbdaf5589742572e1571165
SundarTnCchem <152794104+SundarTnCchem@users.noreply.github.com>
2025-06-07 16:12:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+AOS.init();
+
+const toggle = document.getElementById('dark-mode-toggle');
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggle.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
